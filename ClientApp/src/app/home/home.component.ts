c4970af25104fc0cfa10ca5df63691d981a55c52
Nathan <nlinsley@nadocointeractive.com>
2023-05-18 22:05:18 +1000

    Working on Background Video

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -7,8 +7,23 @@ import { AfterViewInit, Component, ViewChild } from '@angular/core';
 })
 export class HomeComponent implements AfterViewInit {
   @ViewChild('audio') audioRef: any;
+  @ViewChild('videoPlayer') videoPlayerRef: any;
+  @ViewChild('decSeek') decSeekBtnRef: any;
+  @ViewChild('incSeek') incSeekBtnRef: any;
 
   ngAfterViewInit(): void {
     this.audioRef.nativeElement.play();
+
+    const decSeekBtn: HTMLButtonElement = this.decSeekBtnRef.nativeElement;
+    const incSeekBtn: HTMLButtonElement = this.incSeekBtnRef.nativeElement;
+
+    decSeekBtn.onmousedown = () => this.seekByMilliseconds(-2000);
+    incSeekBtn.onmousedown = () => this.seekByMilliseconds(2000);
+  }
+
+  seekByMilliseconds(milliseconds: number): void {
+    const video: HTMLVideoElement = this.videoPlayerRef.nativeElement;
+    const seconds = milliseconds / 1000; // Convert milliseconds to seconds
+    video.currentTime += seconds; // Seek by the desired time interval in seconds
   }
 }
